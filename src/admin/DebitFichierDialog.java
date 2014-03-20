15b6ea874abe199a5ebe84e092948058243cc1c7
Thierry Deo <thierry.deo@gmail.com>
2014-03-20 10:52:49 +0100

    new way of throwing authexception

--- src/admin/DebitFichierDialog.java
+++ src/admin/DebitFichierDialog.java
@@ -30,6 +30,7 @@ import javax.swing.filechooser.FileFilter;
 import javax.swing.table.DefaultTableColumnModel;
 import javax.swing.table.DefaultTableModel;
 import main.Admin;
+import main.AuthException;
 import main.MainWindow;
 import main.Transaction;
 import main.TrigException;
@@ -136,203 +137,202 @@ public class DebitFichierDialog extends JDialog {
 
 		AuthentificationDialog authentification = new AuthentificationDialog(parent);
 		authentification.executer();
-		if (authentification.admin.has_droit("debit_fichier")) {
-			int banqueId = parent.trigrammeActif.id;
-			Admin adminId = authentification.admin;
+		if (!authentification.admin.has_droit("debit_fichier")) {
+			throw new AuthException();
+		}
+		int banqueId = parent.trigrammeActif.id;
+		Admin adminId = authentification.admin;
 
-			JFileChooser chooser = new JFileChooser();
-			chooser.setFileFilter(new csvFileFilter());
-			int returnVal = chooser.showOpenDialog(parent);
-			if (returnVal == JFileChooser.APPROVE_OPTION) {
-				String fichier = chooser.getSelectedFile().getAbsolutePath();
+		JFileChooser chooser = new JFileChooser();
+		chooser.setFileFilter(new csvFileFilter());
+		int returnVal = chooser.showOpenDialog(parent);
+		if (returnVal == JFileChooser.APPROVE_OPTION) {
+			String fichier = chooser.getSelectedFile().getAbsolutePath();
 
-				this.addKeyListener(listener);
+			this.addKeyListener(listener);
 
-				resultats = new JTable();
-				resultats.addKeyListener(listener);
-				resultats.setModel(modele);
+			resultats = new JTable();
+			resultats.addKeyListener(listener);
+			resultats.setModel(modele);
 
-				resultatsScrollPane = new JScrollPane(resultats);
-				resultatsScrollPane
-						.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-				resultatsScrollPane.setPreferredSize(new Dimension(590, 560));
+			resultatsScrollPane = new JScrollPane(resultats);
+			resultatsScrollPane
+					.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+			resultatsScrollPane.setPreferredSize(new Dimension(590, 560));
 
-				String[] header = { "", "Trigramme", "Nom", "Montant", "Commentaire" };
-				modele.setColumnIdentifiers(header);
+			String[] header = { "", "Trigramme", "Nom", "Montant", "Commentaire" };
+			modele.setColumnIdentifiers(header);
 
-				resultats.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-				resultats.getColumnModel().getColumn(0).setPreferredWidth(15);
-				resultats.getColumnModel().getColumn(1).setPreferredWidth(50);
-				resultats.getColumnModel().getColumn(2).setPreferredWidth(100);
-				resultats.getColumnModel().getColumn(3).setPreferredWidth(30);
-				resultats.getColumnModel().getColumn(4).setPreferredWidth(250);
+			resultats.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+			resultats.getColumnModel().getColumn(0).setPreferredWidth(15);
+			resultats.getColumnModel().getColumn(1).setPreferredWidth(50);
+			resultats.getColumnModel().getColumn(2).setPreferredWidth(100);
+			resultats.getColumnModel().getColumn(3).setPreferredWidth(30);
+			resultats.getColumnModel().getColumn(4).setPreferredWidth(250);
 
-				InputStream ips = new FileInputStream(fichier);
-				InputStreamReader ipsr = new InputStreamReader(ips);
-				BufferedReader br = new BufferedReader(ipsr);
-				String ligne;
+			InputStream ips = new FileInputStream(fichier);
+			InputStreamReader ipsr = new InputStreamReader(ips);
+			BufferedReader br = new BufferedReader(ipsr);
+			String ligne;
 
-				while ((ligne = br.readLine()) != null) {
-					char separateur = ',';
-					int virgules = MainWindow.countOccurrences(ligne, ',');
-					int pointvirgules = MainWindow.countOccurrences(ligne, ';');
-					int deuxpoints = MainWindow.countOccurrences(ligne, ':');
-					int espaces = MainWindow.countOccurrences(ligne, ' ');
-					int tabs = MainWindow.countOccurrences(ligne, '\t');
+			while ((ligne = br.readLine()) != null) {
+				char separateur = ',';
+				int virgules = MainWindow.countOccurrences(ligne, ',');
+				int pointvirgules = MainWindow.countOccurrences(ligne, ';');
+				int deuxpoints = MainWindow.countOccurrences(ligne, ':');
+				int espaces = MainWindow.countOccurrences(ligne, ' ');
+				int tabs = MainWindow.countOccurrences(ligne, '\t');
 
-					int best =
-							Math.max(Math.max(virgules, pointvirgules),
-									Math.max(deuxpoints, Math.max(espaces, tabs)));
-					if (best == virgules) {
-						separateur = ',';
-					} else if (best == pointvirgules) {
-						separateur = ';';
-					} else if (best == deuxpoints) {
-						separateur = ':';
-					} else if (best == espaces) {
-						separateur = ' ';
-					} else if (best == tabs) {
-						separateur = '\t';
-					}
-
-					String[] valeurs = ligne.split(separateur + "");
-					if (valeurs.length == 4) {
-						valeurs[2] = valeurs[2] + '.' + valeurs[3];
-					}
-					if (valeurs.length == 3) {
-						Object[] futureLigne =
-								{ true, valeurs[0], valeurs[1], valeurs[2].replace(',', '.'), "" };
-						modele.addRow(futureLigne);
-					} else {
-						Object[] futureLigne = { false, "", "Illisible", "", ligne };
-						modele.addRow(futureLigne);
-					}
-
-				}
-				br.close();
-
-				lignesInterdites = new LinkedList<Integer>();
-
-				for (int i = 0; i < modele.getRowCount(); i++) {
-					int erreur = -1;
-					String comment = "";
-					try {
-						Trigramme trigramme =
-								new Trigramme(parent, (String) modele.getValueAt(i, 1));
-						int montant =
-								(int) (100 * Double.parseDouble((String) modele.getValueAt(i, 3)));
-						if (!trigramme.name.toUpperCase().equals(
-								modele.getValueAt(i, 2).toString().toUpperCase())) {
-							erreur = 1;
-							comment = "Mauvais nom : " + trigramme.name;
-						} else if (montant < 0) {
-							lignesInterdites.add(i);
-							erreur = 2;
-							comment = "Montant négatif";
-						} else if (trigramme.status != 0 && trigramme.balance < montant) {
-							erreur = 3;
-							comment =
-									"Compte non X en négatif ("
-											+ (double) (trigramme.balance - montant) / 100 + ")";
-						} else if (montant > 2000) {
-							erreur = 4;
-							comment = "Montant élevé";
-						} else if (trigramme.balance < montant && trigramme.balance >= 0) {
-							erreur = 5;
-							comment =
-									"Le compte passe en négatif ("
-											+ (double) (trigramme.balance - montant) / 100 + ")";
-						}
-						modele.setValueAt(comment, i, 4);
-						if (erreur >= 1 && erreur <= 3) {
-							modele.setValueAt(Boolean.FALSE, i, 0);
-						}
-
-					} catch (Exception e) {
-						comment = "Trigramme inexistant";
-						lignesInterdites.add(i);
-						modele.setValueAt(Boolean.FALSE, i, 0);
-						modele.setValueAt(comment, i, 4);
-					}
-
+				int best =
+						Math.max(Math.max(virgules, pointvirgules),
+								Math.max(deuxpoints, Math.max(espaces, tabs)));
+				if (best == virgules) {
+					separateur = ',';
+				} else if (best == pointvirgules) {
+					separateur = ';';
+				} else if (best == deuxpoints) {
+					separateur = ':';
+				} else if (best == espaces) {
+					separateur = ' ';
+				} else if (best == tabs) {
+					separateur = '\t';
 				}
 
-				resultats.setModel(modele);
-				resultats.repaint();
-				resultatsScrollPane.repaint();
-				this.repaint();
-
-				validerButton = new JButton("Valider");
-				validerButton.setPreferredSize(new Dimension(200, 20));
-				validerButton.addActionListener(listener);
-
-				fermerButton = new JButton("Fermer la fenêtre");
-				fermerButton.setPreferredSize(new Dimension(200, 20));
-				fermerButton.addActionListener(listener);
-
-				fond = new JPanel();
-				fond.setLayout(new FlowLayout(FlowLayout.CENTER));
-				fond.add(resultatsScrollPane);
-				fond.add(validerButton);
-				fond.add(fermerButton);
-
-				fond.setPreferredSize(new Dimension(600, 600));
-				fond.setOpaque(true);
-
-				this.setContentPane(fond);
-				this.pack();
-				this.setLocation((parent.getWidth() - this.getWidth()) / 2,
-						(parent.getHeight() - this.getHeight()) / 2);
-				this.setResizable(false);
-				this.setVisible(true);
-
-				if (validation) {
-
-					PrintWriter out =
-							new PrintWriter(new BufferedWriter(new FileWriter(fichier.replace(
-									".csv", "") + "_refuses.csv", true)));
-					int refuses = 0;
-
-					for (int i = 0; i < modele.getRowCount(); i++) {
-
-						if (modele.getValueAt(i, 0).equals(Boolean.TRUE)) {
-							int montant =
-									(int) (100 * Double.parseDouble((String) modele
-											.getValueAt(i, 3)));
-							Trigramme trigramme =
-									new Trigramme(parent, (String) modele.getValueAt(i, 1));
-
-							if (!parent.banqueBobActif) {
-								banqueId = parent.banqueBinet.id;
-							}
-							Transaction transaction =
-									new Transaction(trigramme.id, -montant, "", adminId, null,
-											banqueId);
-							transaction.WriteToDB(parent);
-						} else {
-							out.println(modele.getValueAt(i, 1) + "," + modele.getValueAt(i, 2)
-									+ "," + modele.getValueAt(i, 3) + "," + modele.getValueAt(i, 4));
-							refuses++;
-						}
-
-					}
-					out.close();
-
-					if (refuses == 0) {
-						JOptionPane.showMessageDialog(parent, "Opération terminéé", "",
-								JOptionPane.INFORMATION_MESSAGE, null);
-					} else {
-						JOptionPane.showMessageDialog(parent,
-								"Les " + refuses + " lignes ignorées ont été stockées dans "
-										+ fichier.replace(".csv", "") + "_refuses.csv", "",
-								JOptionPane.INFORMATION_MESSAGE, null);
-					}
-
-					parent.refresh();
-
+				String[] valeurs = ligne.split(separateur + "");
+				if (valeurs.length == 4) {
+					valeurs[2] = valeurs[2] + '.' + valeurs[3];
+				}
+				if (valeurs.length == 3) {
+					Object[] futureLigne =
+							{ true, valeurs[0], valeurs[1], valeurs[2].replace(',', '.'), "" };
+					modele.addRow(futureLigne);
+				} else {
+					Object[] futureLigne = { false, "", "Illisible", "", ligne };
+					modele.addRow(futureLigne);
 				}
 
 			}
+			br.close();
+
+			lignesInterdites = new LinkedList<Integer>();
+
+			for (int i = 0; i < modele.getRowCount(); i++) {
+				int erreur = -1;
+				String comment = "";
+				try {
+					Trigramme trigramme = new Trigramme(parent, (String) modele.getValueAt(i, 1));
+					int montant =
+							(int) (100 * Double.parseDouble((String) modele.getValueAt(i, 3)));
+					if (!trigramme.name.toUpperCase().equals(
+							modele.getValueAt(i, 2).toString().toUpperCase())) {
+						erreur = 1;
+						comment = "Mauvais nom : " + trigramme.name;
+					} else if (montant < 0) {
+						lignesInterdites.add(i);
+						erreur = 2;
+						comment = "Montant négatif";
+					} else if (trigramme.status != 0 && trigramme.balance < montant) {
+						erreur = 3;
+						comment =
+								"Compte non X en négatif ("
+										+ (double) (trigramme.balance - montant) / 100 + ")";
+					} else if (montant > 2000) {
+						erreur = 4;
+						comment = "Montant élevé";
+					} else if (trigramme.balance < montant && trigramme.balance >= 0) {
+						erreur = 5;
+						comment =
+								"Le compte passe en négatif ("
+										+ (double) (trigramme.balance - montant) / 100 + ")";
+					}
+					modele.setValueAt(comment, i, 4);
+					if (erreur >= 1 && erreur <= 3) {
+						modele.setValueAt(Boolean.FALSE, i, 0);
+					}
+
+				} catch (Exception e) {
+					comment = "Trigramme inexistant";
+					lignesInterdites.add(i);
+					modele.setValueAt(Boolean.FALSE, i, 0);
+					modele.setValueAt(comment, i, 4);
+				}
+
+			}
+
+			resultats.setModel(modele);
+			resultats.repaint();
+			resultatsScrollPane.repaint();
+			this.repaint();
+
+			validerButton = new JButton("Valider");
+			validerButton.setPreferredSize(new Dimension(200, 20));
+			validerButton.addActionListener(listener);
+
+			fermerButton = new JButton("Fermer la fenêtre");
+			fermerButton.setPreferredSize(new Dimension(200, 20));
+			fermerButton.addActionListener(listener);
+
+			fond = new JPanel();
+			fond.setLayout(new FlowLayout(FlowLayout.CENTER));
+			fond.add(resultatsScrollPane);
+			fond.add(validerButton);
+			fond.add(fermerButton);
+
+			fond.setPreferredSize(new Dimension(600, 600));
+			fond.setOpaque(true);
+
+			this.setContentPane(fond);
+			this.pack();
+			this.setLocation((parent.getWidth() - this.getWidth()) / 2,
+					(parent.getHeight() - this.getHeight()) / 2);
+			this.setResizable(false);
+			this.setVisible(true);
+
+			if (validation) {
+
+				PrintWriter out =
+						new PrintWriter(new BufferedWriter(new FileWriter(fichier.replace(".csv",
+								"") + "_refuses.csv", true)));
+				int refuses = 0;
+
+				for (int i = 0; i < modele.getRowCount(); i++) {
+
+					if (modele.getValueAt(i, 0).equals(Boolean.TRUE)) {
+						int montant =
+								(int) (100 * Double.parseDouble((String) modele.getValueAt(i, 3)));
+						Trigramme trigramme =
+								new Trigramme(parent, (String) modele.getValueAt(i, 1));
+
+						if (!parent.banqueBobActif) {
+							banqueId = parent.banqueBinet.id;
+						}
+						Transaction transaction =
+								new Transaction(trigramme.id, -montant, "", adminId, null, banqueId);
+						transaction.WriteToDB(parent);
+					} else {
+						out.println(modele.getValueAt(i, 1) + "," + modele.getValueAt(i, 2) + ","
+								+ modele.getValueAt(i, 3) + "," + modele.getValueAt(i, 4));
+						refuses++;
+					}
+
+				}
+				out.close();
+
+				if (refuses == 0) {
+					JOptionPane.showMessageDialog(parent, "Opération terminéé", "",
+							JOptionPane.INFORMATION_MESSAGE, null);
+				} else {
+					JOptionPane.showMessageDialog(
+							parent,
+							"Les " + refuses + " lignes ignorées ont été stockées dans "
+									+ fichier.replace(".csv", "") + "_refuses.csv", "",
+							JOptionPane.INFORMATION_MESSAGE, null);
+				}
+
+				parent.refresh();
+
+			}
+
 		}
 	}
 }
