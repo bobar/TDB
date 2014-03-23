997c6e50413d2af0dec50dae3e492f4da959aa8e
Thierry Deo <thierry.deo@gmail.com>
2014-03-23 16:05:53 +0100

    Signed-off-by: francois <francois@francois-K53SD>

--- src/main/Database.java
+++ src/main/Database.java
@@ -2,15 +2,22 @@ package main;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Database {
 
+	static String user_id;
+	static String password;	
+	static String host_name;
+	static String basename;
+	
 	Connection connexion;
 
 	private String getExecutionPath() {
@@ -24,34 +31,41 @@ public class Database {
 		return absolutePath;
 	}
 
+	public void LoadDatabaseConfiguration(String config_file_path) throws IOException{
+		
+		Properties properties=null;
+		InputStream ips=new FileInputStream(config_file_path);
+		
+		properties.load(ips);
+		
+		Database.user_id=properties.getProperty("user");
+		Database.password=properties.getProperty("password");
+		Database.host_name=properties.getProperty("host");
+		Database.basename=properties.getProperty("tdbase");
+	}
+	
 	public void connecter() throws SQLException, ClassNotFoundException, IOException {
 		Class.forName("com.mysql.jdbc.Driver");
 		String user = "root";
-		String passwd = "";
+		String passwd = "plap";
 		String host = "localhost";
 		String db = "tdb";
+		
+		Properties properties=new Properties();
+		
+		
 		String absolutePath = this.getExecutionPath();
-		InputStream ips = new FileInputStream(absolutePath + "/src//TDB.config");
-		InputStreamReader ipsr = new InputStreamReader(ips);
-		BufferedReader br = new BufferedReader(ipsr);
-		String ligne;
-		while ((ligne = br.readLine()) != null) {
-			if (ligne.charAt(0) != '#') {
-				int pos = ligne.indexOf('=');
-				String debut = ligne.substring(0, pos).trim();
-				String fin = ligne.substring(pos + 1, ligne.length()).trim();
-				if (debut.equals("dbUser") && fin.length() > 0) {
-					user = fin;
-				} else if (debut.equals("dbPassword") && fin.length() > 0) {
-					passwd = fin;
-				} else if (debut.equals("dbHost") && fin.length() > 0) {
-					host = fin;
-				} else if (debut.equals("dbName") && fin.length() > 0) {
-					db = fin;
-				}
-			}
-		}
-		br.close();
+		InputStream ips = new FileInputStream(absolutePath + "/src/TDB.config");
+		System.out.println(absolutePath + "/src/TDB.config");
+		
+		properties.load(ips);
+		
+		user=properties.getProperty("dbUser");
+		passwd=properties.getProperty("dbPassword");
+		host=properties.getProperty("dbHost");
+		db=properties.getProperty("dbName");
+		
+		
 		this.connexion =
 				DriverManager.getConnection("jdbc:mysql://" + host + "/" + db, user, passwd);
 	}
---
+++ src/tdb/trig/TrigrammeDatabaseInterface.java
@@ -0,0 +1,7 @@
+package tdb.trig;
+
+public interface TrigrammeDatabaseInterface {
+	
+	
+
+}
